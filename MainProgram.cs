b0c0ffcf6d4d4bc972afa59ed3904c110a30303b
Paweł Grobelny <grobcio@mixbox.pl>
2022-10-24 21:25:56 +0200

    Add project files.

---
+++ MainProgram.cs
@@ -0,0 +1,12 @@
+﻿namespace battleships
+
+{
+    internal class MainProgram
+    {
+        static void Main(string[] args) // app starts here
+        {
+            Game game = new();
+            game.Launch(5, 4, 4); // launch the game with given ships
+        }
+    }
+}
